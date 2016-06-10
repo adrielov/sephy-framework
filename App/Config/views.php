ffdac25a87986baa1cd90ba36a7d5fa805aa5a5c
Adriel Oliveira <adritis.pessoal@gmail.com>
2016-06-09 23:21:09 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- App/Config/views.php
+++ App/Config/views.php
@@ -2,18 +2,18 @@
 
 return [
 
-	/*
+    /*
      * To render your views, you can use the engines: blade, twig
      */
-	'engine' => 'blade', // use blade or twig
+    'engine' => 'blade', // use blade or twig
 
-	/**
-	 * Folder where you stored the views
-	 */
-	'path_views' => ROOT_APP . 'Views',
+    /*
+     * Folder where you stored the views
+     */
+    'path_views' => ROOT_APP.'Views',
 
-	/**
-	 * Folder where you stored the views caches
-	 */
-	'path_cache' => ROOT_APP . 'Cache'
+    /*
+     * Folder where you stored the views caches
+     */
+    'path_cache' => ROOT_APP.'Cache',
 ];
